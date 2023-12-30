820c90e43b647b9ef623207e573699a575850d2d
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-30 22:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateSizeInfo() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  const sizeInfoElement = document.getElementById('windowSize');
+  sizeInfoElement.textContent = `Width: ${width}px, Height: ${height}px`;
+}
+
+// Initial size
+updateSizeInfo();
+
+// Event listener for window resize
+window.addEventListener('resize', updateSizeInfo);
